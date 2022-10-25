35142e97a5653ea68d9d25c726ca407b1c113042
markos <markos.paltser@voco.ee>
2022-10-25 13:28:30 +0300

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -8,12 +8,21 @@ function weatherDataFetch(city) {
             return resp.json()
         })
         .then(function (data) {
-            console.log(data)
+            console.log(data);
+            drawWeather(data);
         })
         .catch(function (){
 
         })
 }
 function cityWeather(e){
-    weatherDataFetch('Tallinn')
+    weatherDataFetch('Tartu')
+}
+function drawWeather(data) {
+    var celcius = Math.round(parseFloat(data.main.temp)-273.15);
+    var description = data.weather[0].description;
+
+    document.querySelector('#description').innerHTML = description;
+    document.querySelector('#temp').innerHTML = celcius + `&deg;`;
+    document.querySelector('#location').innerHTML = data.name;
 }
